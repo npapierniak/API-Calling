bfb8a1fe60e33ff17a1a2bb16f7cd7ce49e53d3c
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-03-08 09:49:18 -0600

    ContentView

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -8,19 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var memes = [Item]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List (memes) { item in
+                Link (destination: URL(string: item.url)!) {
+                    Text (item.name)
+                }
+                .navigationTitle ("Epic Memes")
+            }
+            .task {
+                await loadData ()
+            }
         }
-        .padding()
+    }
+    func loadData() async {
+        if let url = URL(string: "https://api.imgflip.com/get_memes") {
+            if let (data, _) = try? await URLSession.shared.data (from: url) {
+                if let decodedResponse = try? JSONDecoder().decode (Entries.self, from: data) {
+                    memes = decodedResponse.response.memes
+                }
+            }
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct Item: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case url
+    }
+}
+struct Memes: Codable {
+    var memes: [Item]
+}
+struct Entries: Codable {
+    var response: Memes
+    enum CodingKeys: String, CodingKey {
+        case response = "data"
     }
 }
