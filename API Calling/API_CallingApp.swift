7fa309b07b39b38fedf3de179e474ede01979b30
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-03-08 09:36:59 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Nicholas Papierniak on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
